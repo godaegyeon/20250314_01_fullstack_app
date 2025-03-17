1b44324083ab8f322871574077cd5555ab542fa7
Go Daegyeon <dae.gyeon.go@gmail.com>
2025-03-17 10:07:50 +0900

    git first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container, Form } from 'react-bootstrap';
+
+function App() {
+  return (
+    <>
+    <Container>
+      <h1>폼</h1>
+      <Form>
+        <input type='text' />
+        <button type='summit'>검색</button>
+      </Form>
+    </Container>
+    </>
+  );
+}
+
+export default App;
