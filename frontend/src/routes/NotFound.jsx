733f7a90e613057942b743f86430e41481ae2e42
GoDaeGyeon <dae.gyeon.go@gmail.com>
2025-03-24 11:39:43 +0900

    오류 페이지 수정 404 이미지 파일 적용

--- frontend/src/routes/NotFound.jsx
+++ frontend/src/routes/NotFound.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-// import Error from '../../public/404_not_found.png'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Error from '../../public/404_not_found.png';
 
 function NotFound() {
   return (
     <>
       <div>404 NotFound : 패이지를 찾을수없습니다.</div>
-      <Error/>
+      <div>
+        <img src={Error} alt="error" style={{width:'1000px'}} />
+      </div>
     </>
-  )
+  );
 }
 
-export default NotFound
+export default NotFound;
